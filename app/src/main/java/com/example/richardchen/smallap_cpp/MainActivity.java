df66dc3d30f823dc531148100c6dd1fab5421e2f
RichardChen <richardchen@via.com.tw>
2017-06-08 15:56:33 +0800

    Add code for testing (Direct) ByteBuffer.

--- app/src/main/java/com/example/richardchen/smallap_cpp/MainActivity.java
+++ app/src/main/java/com/example/richardchen/smallap_cpp/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.nio.ByteBuffer;
+
 public class MainActivity extends Activity {
 
     // Used to load the 'native-lib' library on application startup.
@@ -16,14 +18,27 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        ByteBuffer buffer = ByteBuffer.allocate(10);
+        ByteBuffer buffer = ByteBuffer.allocateDirect(10);
+
+        byte   b1 = 'A';
+        byte[] b2 = new byte[]{'B', 'C', 'D'};
+        byte[] b3 = {'E', 'F', 'G'};
+        buffer.put(b1);
+        buffer.put(b2);
+        buffer.put(b3);
+        char ch0 = (char) buffer.get(0);
+        char ch2 = (char) buffer.get(2);
+
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-    }
+        tv.setText(stringFromJNI(buffer));
+        ch0 = (char) buffer.get(0);
 
+    }
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+    public native String stringFromJNI(ByteBuffer buffer);
 }
